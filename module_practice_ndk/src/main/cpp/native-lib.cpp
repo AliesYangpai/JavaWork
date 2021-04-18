9bf77904967c6eef701dcf5e505bb0d8f41c7e6b
AliesYangpai <yangpai_beibei@163.com>
2021-04-18 20:52:25 +0800

    add practice of ndk (this is a small demo still need to practice)

---
+++ module_practice_ndk/src/main/cpp/native-lib.cpp
@@ -0,0 +1,60 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+extern "C"
+
+JNIEXPORT jstring JNICALL
+Java_com_alie_modulepracticendk_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject thiz) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+void showArrayData(jint *p, jsize size) {
+    for (int i = 0; i < size; i++) {
+        __android_log_print(ANDROID_LOG_DEBUG, "XXX", "===jniData native array[%d] = %d", i,
+                            *(p + i));
+    }
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_alie_modulepracticendk_MainActivity_do_1Test_101_1print_1data(
+        JNIEnv *env,
+        jobject thiz,
+        jshort age,
+        jstring name,
+        jintArray array) {
+    __android_log_print(ANDROID_LOG_DEBUG, "XXX", "===jniData native age:%d", age);
+    /**
+     * isCopy
+     * 1:copy to a new data in memory ,the pointer change to a new one
+     * 0:not create value just using the data that point to java (from java)
+     */
+    const char *p_name = env->GetStringUTFChars(name, NULL);
+    __android_log_print(ANDROID_LOG_DEBUG, "XXX", "===jniData native name:%s", p_name);
+
+    jint *p_array = env->GetIntArrayElements(array, NULL);
+    jsize length = env->GetArrayLength(array);
+    for (int i = 0; i < length; i++) {
+        __android_log_print(ANDROID_LOG_DEBUG, "XXX", "===jniData native array[%d] = %d", i,
+                            *(p_array + i));
+    }
+
+    /**
+     * ============================Release================================
+     * When you env->GetXXXXX() you also have to env-> Releasexxxxx() if needed
+     */
+    env->ReleaseStringUTFChars(name, p_name);
+
+    /**
+     * 参数3：MODE
+     * 0:刷新java数组 并释放C/C++数组
+     * 1:JNI_COMMIT -> 只刷新java数组，不释放
+     * 2:JNI_ABORT -> java c++ 全部释放
+     */
+    env->ReleaseIntArrayElements(array, p_array,0);
+}
+
---
+++ module_practice_ndk/src/main/java/com/alie/modulepracticendk/MainActivity.java
@@ -0,0 +1,56 @@
+package com.alie.modulepracticendk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Reference to learn ：
+ * https://www.jianshu.com/p/4c890e23c021
+ */
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponent();
+    }
+
+    void initComponent() {
+        findViewById(R.id.btn1).setOnClickListener(v -> {
+            String content = stringFromJNI();
+            Log.d(TAG,"===jniData java initComponent :"+content);
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(v -> {
+            short age = 55;
+            String name = "alie";
+            int[] array = {1,2,3,4,5,6};
+            do_Test_01_print_data(age,name,array);
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+
+    /**
+     * do_Test_01 打印参数
+     * @param age
+     * @param name
+     * @param array
+     */
+    public native void do_Test_01_print_data(short age,String name,int[] array);
+}
